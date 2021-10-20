efd578364772ba6e5c5dfacbdc772e9b620d595c
ohs95 <dhgustjd10@gmail.com>
2021-10-20 17:54:28 +0900

    test

---
+++ src/main/java/com/onetrillion/trip/wishlist/WishListCriteria.java
@@ -0,0 +1,78 @@
+package com.onetrillion.trip.wishlist;
+
+import com.onetrillion.trip.page.Criteria;
+
+public class WishListCriteria implements Criteria {
+	
+	private int page;
+	private int perPageNum;
+	private int rowStart;
+	private int rowEnd;
+	
+	
+	 public WishListCriteria()
+	 {
+	  this.page = 1;
+	  this.perPageNum = 5;
+	 }
+
+	 @Override
+	public void setPage(int page)
+	 {
+	  if (page <= 0)
+	  {
+	   this.page = 1;
+	   return;
+	  }
+	  this.page = page;
+	 }
+
+	 @Override
+	public void setPerPageNum(int perPageNum)
+	 {
+	  if (perPageNum <= 0 || perPageNum > 100)
+	  {
+	   this.perPageNum = 5;
+	   return;
+	  }
+	  this.perPageNum = perPageNum;
+	 }
+
+	 @Override
+	public int getPage()
+	 {
+	  return page;
+	 }
+
+	 @Override
+	public int getPageStart()
+	 {
+	  return (this.page - 1) * perPageNum;
+	 }
+
+	 @Override
+	public int getPerPageNum()
+	 {
+	  return this.perPageNum;
+	 }
+
+	 @Override
+	 public String toString() {
+	  return "Criteria [page=" + page + ", perPageNum=" + perPageNum + ""
+	    + ", rowStart=" +  getRowStart() + ", rowEnd=" + getRowEnd()
+	    + "]";
+	 }
+
+	 @Override
+	public int getRowStart() {
+	  rowStart = ((page - 1) * perPageNum) + 1;
+	  return rowStart;
+	 }
+
+	 @Override
+	public int getRowEnd() {
+	  rowEnd = rowStart + perPageNum - 1;
+	  return rowEnd;
+	 }
+	
+}
